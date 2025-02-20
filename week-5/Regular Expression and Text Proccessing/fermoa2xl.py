44b31c3a65547f281c40ce7699a99d25f821315b
deepanshuTTN <deepanshu.mishra@tothenew.com>
2025-02-20 22:37:16 +0530

    completed fermosa scraper

--- week-5/Regular Expression and Text Proccessing/fermoa2xl.py
+++ week-5/Regular Expression and Text Proccessing/fermoa2xl.py
@@ -4,79 +4,158 @@ import requests
 import re
 
 
+
 class Sansevierias:
-    def __init__(self, base_url, ws):
+    def __init__(self, base_url, ws=False):
+        '''
+        Class object thats used to scrape the fermosa webpage\n
+        Setup base url for scraping\n
+        An opened worksheet for appending scraped data\n
+        Instance attributes: max_names, base_url, domain, patterns
+        '''
+        self.max_names = 0
         self.domain = "https://fermosaplants.com"
         self.base_url = base_url
         self.ws = ws
         self.patterns = {
-            'is_Variegated': re.compile(r'variegated', re.IGNORECASE),
-            'listing_combo_amount': re.compile(r'combo.*of\s([0-9]+)', re.IGNORECASE),
-            'listing_type': re.compile(r'combo|clump|Leaf|plant|pub', re.IGNORECASE)
+            #get if a plant is variegated
+            'is_Variegated': re.compile(r'variegated', re.IGNORECASE).search, 
+
+            #get all the listing types from given types
+            'listing_type': re.compile(r'combo|clump|Leaf|plant|pub', re.IGNORECASE).findall, 
+
+            #get a floating point number from entered price
+            'extract_price': re.compile(r'(Rs\.|\s|,)').sub, 
+
+            #try to fetch name for a single listing
+            'extract_name': re.compile(r'''(?:About Sansevieria (?P<name>[\-'"a-zA-Z ]*)(?: )-)''', re.IGNORECASE).search, 
+
+            #second method to fetch name for a single listing
+            'extract_namee': re.compile(r'''(?:Sansevieria (?P<namee>\w+))''').search, 
+
+            #extract nmes from a combo offer listing
+            'extract_names': re.compile(r'\d+\. *(?P<name>[\w]+(?!\.)(?: *(?!\d)[\w])*)', re.IGNORECASE).findall 
         }
+        self.name_set = set()
 
 
     def scrape_page(self, page_url):
+        '''
+        Scrape page function, takes in a fermosa webpage url and scrapes listed products\n
+        returns a list of scraped products data
+        '''
         res = requests.get(page_url)
         soup = BeautifulSoup(res.content, 'html.parser')
         sansevierias = soup.find_all("div", class_="product-item-v5")
         
         #raise error if no items are fetched
         assert len(sansevierias)>0
+        data_scraped = list()
 
         #simple function that returns a true value or the object itself if object exists otherwise a false value
         get_or_default = lambda obj, true, false: true or obj if obj else false
 
         for lising in sansevierias:
 
-            listing_name = lising.find('h4', class_='title-product')
-            listing_price = lising.find('p', class_='price-product')
+            _listing_name = lising.find('h4', class_='title-product')
+            listing_name = _listing_name.text.strip()
 
-            listing_url_ref = listing_name.find('a')['href'] #only 1 level below listing_name object so use it as the parent
-            listing_combo_amount_ref = self.patterns['listing_combo_amount'].search(listing_name.text) #broke to ref since have to get combo number from same object later
+            _listing_price = lising.find('span', class_='price')
+            listing_price = self.patterns['extract_price']('', _listing_price.text.strip())
 
+            listing_url_ref = _listing_name.find('a')['href'] #only 1 level below _listing_name object so use it as the parent
             listing_url = self.domain + listing_url_ref
-            is_Variegated = get_or_default(self.patterns['is_Variegated'].search(listing_name.text), True, False) 
-            listing_combo_amount = get_or_default(listing_combo_amount_ref, None, None)  
-            listing_types = get_or_default(self.patterns['listing_type'].findall(listing_name.text), None, [])
 
+            is_Variegated = get_or_default(self.patterns['is_Variegated'](listing_name), True, False) 
+            listing_types = get_or_default(self.patterns['listing_type'](listing_name), None, [])
+
+            names = self.extract_names(listing_url, 'combo' in listing_name.lower())
+            if names == []:
+                names.append(listing_name.strip())
+            self.name_set = self.name_set.union([name.replace("'", '').title() for name in names])
+
+            self.max_names = max(self.max_names, len(names))
 
             listing_data = [
-                listing_name.text.strip(), 
-                listing_price.text.strip(), 
-                is_Variegated, '1' if not listing_combo_amount else listing_combo_amount.groups()[0], 
+                listing_name, 
+                listing_price, 
+                is_Variegated,
+                len(names), 
                 ', '.join(set(types.lower() for types in listing_types)), 
                 listing_url
-                            ]
-            self.ws.append(listing_data)
-            # print(listing_data)
+                ]
+
+            listing_data.extend([name.replace("'", '').title() for name in names])  
+
+            if self.ws:          
+                self.ws.append(listing_data)
+            data_scraped.append(listing_data)
+
+        return data_scraped
+    
+
+    def extract_names(self, item_url, combo:bool)->list[str]:
+        '''
+        This function extracts names from a given fermosa product webpage url,\n
+        Also Information weather its a single product or a combo and returns a list of names for the listing
+        '''
+        res = requests.get(item_url)
+        soup = BeautifulSoup(res.content, 'html.parser')
+
+        if not combo:
+            _summary = soup.find('div', class_='pd_summary').text
+            summary = re.sub(' ', ' ', _summary)
+            try:
+                name = self.patterns['extract_name'](summary)
+                if name:
+                    return [name.group('name')]
+                else:
+                    return [self.patterns['extract_namee'](summary).group('namee')]
+            except AttributeError:
+                return []
+        
+        else:
+            _description = soup.find('div', class_='desc product-desc').text
+            description = re.sub(' ', ' ', _description)
+            names = self.patterns['extract_names'](description)
+            return sorted(names)
+
 
     def scrape_from(self, page_number):
+        '''Simple function, takes in a page number and calls scrape_page function until products are exhausted'''
         while True:
             try:
                 curr_url = self.base_url+str(page_number)
-                res_flag = requests.get(curr_url)
-                print(page_number)
-                print(res_flag.status_code)
-                self.scrape_page(curr_url)
+                data_entries = self.scrape_page(curr_url)
+                print(f'{len(data_entries)} entries fetched from page {page_number}')
                 page_number+=1
             except AssertionError:
+                #No more products left for fetching, add headers and return
+                headers = ['Product Name', 'Price', 'Variegated', 'Combo Amount', 'Listing Tags', 'Product Url']
+                self.add_headers(headers)
                 break
+        return None
+
+
+    def add_headers(self, headers:list):
+        '''This function adds an empty row at the top and adds valid headers to it'''
+        headers.extend([f'name{n}' for n in range(1, self.max_names+1)])
+        self.ws.insert_rows(1)
+        for col, heading in enumerate(headers):
+            self.ws.cell(row=1, column=col+1, value=heading)
+        return None
 
 
 if __name__ == '__main__':
-    #cant save the file twice
-    wb = Workbook(write_only=True)
-
-    #get the active sheet or none
+    wb = Workbook()
     ws = wb.create_sheet("Sansevierias")
 
-    #add headings
-    ws.append(['Product Name', 'Price', 'Variegated', 'Combo Amount', 'Listing Types', 'Product Url'])
-
     base_url = "https://fermosaplants.com/collections/sansevieria?page="
-    fermosa_scraper = Sansevierias(base_url, ws)
 
+    fermosa_scraper = Sansevierias(base_url, ws)
     fermosa_scraper.scrape_from(page_number=1)
+    print(f'{len(fermosa_scraper.name_set)} distinct values found!')
+    for name in fermosa_scraper.name_set:
+        print(name, end=', ')
 
     wb.save('plantbook.xlsx')
--- week-5/Regular Expression and Text Proccessing/npiPOST.py
+++ week-5/Regular Expression and Text Proccessing/npiPOST.py
@@ -2,6 +2,7 @@ import re
 import requests
 import json
 import time
+import concurrent.futures as concurrent_features
 
 
 def get_NPI_list(npi_file):
@@ -14,27 +15,47 @@ def get_NPI_list(npi_file):
         return re.findall(r'[0-9]{10}', f.read())
     
 
-def generate_npi_objects(npi_list):
+def generate_npi_json(npi_list, output_json):
     '''
     This function gets information based on the input npi numbers list\n
     Takes in a list of npi numbers, writes the outputs in out.json file and returns the dictionary of the objects
     '''
-    return_dict = dict()
+    return_list = list()
     for npi in npi_list:
         r = requests.post("https://npiregistry.cms.hhs.gov/RegistryBack/npiDetails", json={"number":npi})
 
         #(number:npi_object) key pair is added where only non empty outer most fields of the objects exist
-        return_dict[npi] = dict([(key, value) for key, value in r.json().items() if value])
+        return_list.append(dict([(key, value) for key, value in r.json().items() if value]))
 
-    with open('out.json', 'a') as out:
-        json.dump(return_dict, out, indent=4)
+    with open(output_json, 'a') as out:
+        json.dump(return_list, out, indent=4)
     
-    return return_dict
+    return return_list
 
 
-npilist = (get_NPI_list('.npis'))
-start_time = time.time()
-generate_npi_objects(npilist)
-endtime = time.time()
-print(endtime-start_time)
-print(endtime, start_time)
+def get_npi_object(npi_id):
+    '''
+    This function gets an object corresponding to npi number input\n
+    returns a dictionary or NONE
+    '''
+    res = requests.post("https://npiregistry.cms.hhs.gov/RegistryBack/npiDetails", json={"number":npi_id})
+    if res.json()=={}:
+        return
+
+    #(number:npi_object) key pair is added where only non empty outer most fields of the objects exist
+    return dict((key,value) for key, value in res.json().items() if value)
+    
+
+
+if __name__ == '__main__':
+    npilist = (get_NPI_list('.npis'))
+    start_time = time.time()
+    with concurrent_features.ThreadPoolExecutor(max_workers=40) as executor:
+        dic_list = list(i for i in executor.map(get_npi_object, npilist) if i)
+
+    with open('npi_objects.json', 'w') as out:
+        json.dump(dic_list, out, indent=4)
+
+    endtime = time.time()
+    print(endtime-start_time)
+    print(endtime, start_time)
